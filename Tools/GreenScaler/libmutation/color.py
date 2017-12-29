42b9f7475b8805d7e20699e45c9a673803c33f7d
msacbd@gmail.com <msacbd@gmail.com>
2017-12-29 03:13:58 -0700

    fixes

--- Tools/GreenScaler/libmutation/color.py
+++ Tools/GreenScaler/libmutation/color.py
@@ -23,9 +23,13 @@ class RGB:
 
 
 	def pull_images(self):
-		os.system("rm -rf "+utils.IMAGE_PATH+self.package)
+		if os.path.exists(utils.IMAGE_PATH+self.package):
+			os.system("rm -rf "+utils.IMAGE_PATH+self.package)
 		os.system("mkdir "+utils.IMAGE_PATH+self.package)
 		subprocess.call("adb pull /sdcard/screen_shots/ "+utils.IMAGE_PATH+self.package, shell=True)
+		if not os.path.exists(utils.IMAGE_PATH+self.package+"/screen_shots"):
+			os.system("mkdir "+utils.IMAGE_PATH+self.package+"/screen_shots")
+			os.system("mv  "utils.IMAGE_PATH+self.package+"/* "+utils.IMAGE_PATH+self.package+"/screen_shots/")
 
 	def delete_images(self):
 
